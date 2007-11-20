3e96dc8a0eca88987f85f26ba990ae2e6e6330af
Atle Nissestad <atle@fpgamonster.norbit.no>
2007-11-20 12:07:19 +0100

    Merged cleaned-up nios2-v6.1

--- gcc/config/nios2/nios2-dp-bit.c
+++ gcc/config/nios2/nios2-dp-bit.c
@@ -1,4 +1,7 @@
 
+#ifndef __nios2_big_endian__
+#define FLOAT_BIT_ORDER_MISMATCH
+#endif
 /* This is a software floating point library which can be used
    for targets without hardware floating point. 
    Copyright (C) 1994, 1995, 1996, 1997, 1998, 2000, 2001, 2002, 2003, 2004
